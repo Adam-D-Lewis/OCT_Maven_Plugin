25f01555b6942959ae721c358454c22af485a1b5
Adam Lewis <balast@users.noreply.github.com>
2018-05-22 12:14:04 -0500

    added more to file structure

---
+++ src/main/java/edu/utexas/oct_plugin_ij/run_plugins/Clean.java
@@ -0,0 +1,30 @@
+package edu.utexas.oct_plugin_ij.run_plugins;
+
+import edu.utexas.oct_plugin_ij.OCT_Plugin_Main;
+import net.imagej.ImageJ;
+import org.scijava.command.Command;
+import org.scijava.plugin.Plugin;
+
+@Plugin(type = Command.class, menuPath = "Plugins>OCT Plugin>Clean")
+public class Clean implements Command{
+
+    public void run() {
+        OCT_Plugin_Main OCT_plugin = new OCT_Plugin_Main();
+        OCT_plugin.run("CLEAN");
+    }
+
+    /**
+     * This main function serves for development purposes.
+     * It allows you to run the plugin immediately out of
+     * your integrated development environment (IDE).
+     *
+     * @param args whatever, it's ignored
+     * @throws Exception
+     */
+    public static void main(final String... args) throws Exception {
+        // create the ImageJ application context with all available services
+        final ImageJ ij = new ImageJ();
+        ij.ui().showUI();        // invoke the plugin
+        ij.command().run(Clean.class, true);
+    }
+}
---
+++ src/main/java/edu/utexas/oct_plugin_ij/run_plugins/run_En_Face_Viewer.java
@@ -0,0 +1,30 @@
+package edu.utexas.oct_plugin_ij.run_plugins;
+
+import edu.utexas.oct_plugin_ij.OCT_Plugin_Main;
+import net.imagej.ImageJ;
+import org.scijava.command.Command;
+import org.scijava.plugin.Plugin;
+
+@Plugin(type = Command.class, menuPath = "Plugins>OCT Plugin>En Face Viewer")
+public class run_En_Face_Viewer implements Command{
+
+    public void run() {
+        OCT_Plugin_Main OCT_plugin = new OCT_Plugin_Main();
+        OCT_plugin.run("En Face Viewer");
+    }
+
+    /**
+     * This main function serves for development purposes.
+     * It allows you to run the plugin immediately out of
+     * your integrated development environment (IDE).
+     *
+     * @param args whatever, it's ignored
+     * @throws Exception
+     */
+    public static void main(final String... args) throws Exception {
+        // create the ImageJ application context with all available services
+        final ImageJ ij = new ImageJ();
+        ij.ui().showUI();        // invoke the plugin
+        ij.command().run(run_En_Face_Viewer.class, true);
+    }
+}
